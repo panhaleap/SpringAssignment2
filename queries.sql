df979324343a545c203738e3c4d4be3a3edf3315
panhaleap <panhaleap@gmail.com>
2019-12-11 15:53:26 +0700

    add backup database and script create database in case we want to run our project else where

---
+++ queries.sql
@@ -0,0 +1,18 @@
+-- Database: faq
+
+-- DROP DATABASE faq;
+
+CREATE DATABASE faq
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.1252'
+    LC_CTYPE = 'English_United States.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
+COMMENT ON DATABASE faq
+    IS 'for 1st Assignment, related to  Web project
+ Tomcat or SpringBoot, Gradle Project, Spring, Controller / Service / Dao, DB transaction of Spring, MyBatis, Postgresql, Javascript, Jquery, Thymeleaf Bootstrap';
+ 
+ 
