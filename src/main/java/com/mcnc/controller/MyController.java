8a613d5e90178afc4812ff757dd41ef528327e00
panhaleap <panhaleap@gmail.com>
2019-12-11 09:56:15 +0700

    test thymeleaf with css

--- src/main/java/com/mcnc/controller/MyController.java
+++ src/main/java/com/mcnc/controller/MyController.java
@@ -3,17 +3,10 @@ package com.mcnc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MyController {
-		
-//    @GetMapping({"/", "/hello"})
-//    public String hello(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
-//        model.addAttribute("name", name);
-//        return "index";
-//    }
-    
+		    
     @GetMapping("/")
     public String index(Model model) {
 
