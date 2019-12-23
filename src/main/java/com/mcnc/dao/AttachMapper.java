78425723468555bcbcfbf34a6d948f1ded9f13ac
panhaleap <panhaleap@gmail.com>
2019-12-23 17:29:01 +0700

    add attach mapper and attact model

---
+++ src/main/java/com/mcnc/dao/AttachMapper.java
@@ -0,0 +1,84 @@
+package com.mcnc.dao;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+import com.mcnc.entity.Attach;
+import com.mcnc.entity.Category;
+import com.mcnc.util.MyBatisUtil;
+
+
+@Repository
+public class AttachMapper {
+	
+//	public void saveCategory(Category category){
+//		SqlSession session = MyBatisUtil.getSqlSessionFactory().openSession();
+//		session.insert("insertCategory", category);
+//		session.commit();
+//		session.close();
+//	}
+//	
+//	public void updateByCode(Category category, String oldCategoryCode){
+//		SqlSession session = MyBatisUtil.getSqlSessionFactory().openSession();
+//		HashMap<String, Object> map=new HashMap<String, Object>();
+//		map.put("category", category);
+//		map.put("oldCategoryCode", oldCategoryCode);		
+//		session.update("updateByCode", map);
+//		session.commit();
+//		session.close();
+//	}
+//	
+//	public void deleteCategory(String categoryCode){
+//		SqlSession session = MyBatisUtil.getSqlSessionFactory().openSession();
+//		session.delete("deleteCategory", categoryCode);
+//		session.commit();
+//		session.close();
+//	}
+//	
+//	public List<Category> getAllCategories(){
+//		SqlSession session = MyBatisUtil.getSqlSessionFactory().openSession();
+//		List<Category> categoryList = session.selectList("getAllCategories");
+//		session.commit();
+//		session.close();
+//		return categoryList;
+//	}
+//	
+	public List<Attach> findById(int boardId){
+		SqlSession session = MyBatisUtil.getSqlSessionFactory().openSession();
+		List<Attach> attachs = session.selectList("findByBoardId", boardId);
+		session.commit();
+		session.close();
+		return attachs;
+	}
+//	
+//	//Test CKEditor Comment
+//	public void saveCategoryTestCKEditor(Category category){
+//		SqlSession session = MyBatisUtil.getSqlSessionFactory().openSession();
+//		session.insert("insertCategoryTestCKEditor", category);
+//		session.commit(); 
+//		session.close();
+//	}
+//	
+//	public Category findByCodeCKEditor(String categoryCode){
+//		SqlSession session = MyBatisUtil.getSqlSessionFactory().openSession();
+//		Category category = (Category) session.selectOne("findByCodeCKEditor", categoryCode);
+//		session.commit();
+//		session.close();
+//		return category;
+//	}
+//	
+//	public void updateByCodeCKEditor(Category category, String oldCategoryCode){
+//		SqlSession session = MyBatisUtil.getSqlSessionFactory().openSession();
+//		HashMap<String, Object> map=new HashMap<String, Object>();
+//		map.put("category", category);
+//		map.put("oldCategoryCode", oldCategoryCode);		
+//		session.update("updateByCodeCKEditor", map);
+//		session.commit();
+//		session.close();
+//	}
+}
---
+++ src/main/java/com/mcnc/entity/Attach.java
@@ -0,0 +1,93 @@
+package com.mcnc.entity;
+
+import java.sql.Blob;
+
+public class Attach extends TrackTimestamp{
+	private int id;
+	private int boardId;
+	private Board board;
+	private String name;
+	private String originalName;
+	private String ext;
+	private Long size;
+	private String contentType;
+	private String path;
+	private Blob data;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getBoardId() {
+		return boardId;
+	}
+	public void setBoardId(int boardId) {
+		this.boardId = boardId;
+	}
+	public Board getBoard() {
+		return board;
+	}
+	public void setBoard(Board board) {
+		this.board = board;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getOriginalName() {
+		return originalName;
+	}
+	public void setOriginalName(String originalName) {
+		this.originalName = originalName;
+	}
+	public String getExt() {
+		return ext;
+	}
+	public void setExt(String ext) {
+		this.ext = ext;
+	}
+	public Long getSize() {
+		return size;
+	}
+	public void setSize(Long size) {
+		this.size = size;
+	}
+	public String getContentType() {
+		return contentType;
+	}
+	public void setContentType(String contentType) {
+		this.contentType = contentType;
+	}
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+	public Blob getData() {
+		return data;
+	}
+	public void setData(Blob data) {
+		this.data = data;
+	}
+	@Override
+	public String toString() {
+		return "Attach [id=" + id 
+				+ ", "+ "boardId=" + boardId 
+				+ ", board=" + board 
+				+ ", name=" + name 
+				+ ", originalName="	+ originalName 
+				+ ", ext=" + ext 
+				+ ", size=" + size 
+				+ ", contentType=" + contentType 
+				+ ", path=" + path
+				+ ", data=" + data 
+				+ ", createdBy()=" + getCreatedBy()
+				+ ", createdDate()=" + getCreatedDate() + "]";
+	}
+	
+	
+}
